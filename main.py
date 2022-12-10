544b2c33c16bb1635222eeadb2fb10e36a2f7fd8
Cristian Gutierrez <cristian.j.gutierrez4@gmail.com>
2022-12-10 16:47:54 -0600

    Finished basic requirements and did code clean up

--- main.py
+++ main.py
@@ -7,27 +7,32 @@ os.system("clear")
 
 menu = Menu()
 coffee_machine = CoffeeMaker()
-latte = menu.menu[0].name
-espresso = menu.menu[1].name
-cappuccino = menu.menu[2].name
+money = MoneyMachine()
+latte = menu.menu[0]
+espresso = menu.menu[1]
+cappuccino = menu.menu[2]
 # print(menu.menu[0].name)
 
 user_request = input("What would you like? (espresso/latte/cappuccino) ")
+coffee_flavor = menu.find_drink(user_request)
 
 if user_request == "off":
     pass
 
 elif user_request == "report":
     coffee_machine.report()
+    money.report()
 
-elif user_request == latte:
-    # latte
-    print(user_request)
+elif menu.find_drink(user_request):
+    drink = menu.find_drink(user_request)
+    make_drink = coffee_machine.is_resource_sufficient(drink)
+    if make_drink:
+        enough_money = money.make_payment(drink.cost)
+        coffee_machine.make_coffee(drink)
 
-elif user_request == espresso:
-    # espresso
-    print(user_request)
 
-elif user_request == cappuccino:
-    # cappuccino
-    print(user_request)
+elif menu.find_drink(user_request) is None:
+    print("Sorry either that item is not available or you have entered an incorrect input.")
+
+
+
